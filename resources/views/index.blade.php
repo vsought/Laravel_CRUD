cdd3457f8131964faf20c6dbb07ea7fefd8b281f
Vinicius Augusto <vinaut.cr@gmail.com>
2021-12-04 00:24:58 -0300

    added a new controller and a new route
    added a new controller and started setting th index route with some templates files

---
+++ resources/views/index.blade.php
@@ -0,0 +1,6 @@
+@extends('templates.template')
+
+@section('content')
+    <h1>Teste</h1>
+    <hr>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BookController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [BookController::class, 'index']);
+//Route::get('series',[SeriesController::class, 'listarSeries']);
